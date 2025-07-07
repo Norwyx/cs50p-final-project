db336d3dad8856a01f40471baac05acf943b44a1
Manuel Salazar <mannisuso@gmail.com>
2025-07-07 18:45:30 -0500

    Create test for the update_master_password function

--- tests/test_database.py
+++ tests/test_database.py
@@ -20,3 +20,11 @@ def test_get_master_password():
     database.set_master_password(conn, hashed_password)
     assert database.get_master_password(conn) == hashed_password
     conn.close()
+
+
+def test_update_master_password():
+    conn = database.get_db_connection(":memory:")
+    database.init_db(conn)
+    database.update_master_password(conn, hashed_password)
+    assert database.get_master_password(conn) == hashed_password
+    conn.close()
