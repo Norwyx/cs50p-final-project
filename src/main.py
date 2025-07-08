028ef8cc1477c4da113d2ece7ddbb4a6a9649d52
Manuel Salazar <mannisuso@gmail.com>
2025-07-07 20:41:05 -0500

    Create basic instance variables for the Class Vault

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 from art import tprint
 
+import database
+import security
+
+
 class Vault:
-    ...
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+        self._encryption_key = None
+
+        database.init_db(self.db_path)
+
 
 def main() -> None:
     ...
